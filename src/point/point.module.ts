4664d820368a5ca2b271fe66fcee45eeddb4bbf4
rivolt0421 <dlrkddnr0421@daum.net>
2024-09-26 00:59:09 +0900

    PointModule provider 등록

--- src/point/point.module.ts
+++ src/point/point.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { PointController } from './point.controller';
 import { DatabaseModule } from 'src/database/database.module';
+import { PointService } from './point.service';
+import { UserPointRepositoryToken } from './user-point.repository';
+import { PointHistoryRepositoryImpl } from 'src/database/point-history.repository.impl';
+import { PointHistoryRepositoryToken } from './point-history.repository';
+import { UserPointRepositoryImpl } from 'src/database/user-point.repository.impl';
 
 @Module({
   imports: [DatabaseModule],
+  providers: [
+    PointService,
+    { provide: UserPointRepositoryToken, useClass: UserPointRepositoryImpl },
+    {
+      provide: PointHistoryRepositoryToken,
+      useClass: PointHistoryRepositoryImpl,
+    },
+  ],
   controllers: [PointController],
 })
 export class PointModule {}
