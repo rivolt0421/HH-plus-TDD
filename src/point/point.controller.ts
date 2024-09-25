f57635828eb6ac1b42a3ba5b1a795bef7f8bac54
rivolt0421 <dlrkddnr0421@daum.net>
2024-09-26 00:59:18 +0900

    PointController 수정

--- src/point/point.controller.ts
+++ src/point/point.controller.ts
@@ -6,17 +6,13 @@ import {
   Patch,
   ValidationPipe,
 } from '@nestjs/common';
-import { PointHistory, TransactionType, UserPoint } from './point.model';
-import { UserPointTable } from 'src/database/userpoint.table';
-import { PointHistoryTable } from 'src/database/pointhistory.table';
 import { PointBody as PointDto } from './point.dto';
+import { PointHistory, UserPoint } from './point.model';
+import { PointService } from './point.service';
 
 @Controller('/point')
 export class PointController {
-  constructor(
-    private readonly userDb: UserPointTable,
-    private readonly historyDb: PointHistoryTable,
-  ) {}
+  constructor(private readonly pointService: PointService) {}
 
   /**
    * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
@@ -24,7 +20,9 @@ export class PointController {
   @Get(':id')
   async point(@Param('id') id): Promise<UserPoint> {
     const userId = Number.parseInt(id);
-    return { id: userId, point: 0, updateMillis: Date.now() };
+    const userPoint = await this.pointService.getUserPoint(userId);
+
+    return userPoint;
   }
 
   /**
@@ -33,7 +31,8 @@ export class PointController {
   @Get(':id/histories')
   async history(@Param('id') id): Promise<PointHistory[]> {
     const userId = Number.parseInt(id);
-    return [];
+
+    return this.pointService.getPointHistories(userId);
   }
 
   /**
@@ -46,7 +45,10 @@ export class PointController {
   ): Promise<UserPoint> {
     const userId = Number.parseInt(id);
     const amount = pointDto.amount;
-    return { id: userId, point: amount, updateMillis: Date.now() };
+
+    const userPoint = await this.pointService.chargePoint(userId, amount);
+
+    return userPoint;
   }
 
   /**
@@ -59,6 +61,9 @@ export class PointController {
   ): Promise<UserPoint> {
     const userId = Number.parseInt(id);
     const amount = pointDto.amount;
-    return { id: userId, point: amount, updateMillis: Date.now() };
+
+    const userPoint = await this.pointService.usePoint(userId, amount);
+
+    return userPoint;
   }
 }
