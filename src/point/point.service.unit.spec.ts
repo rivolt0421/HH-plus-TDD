72cb8b283d20dbb5958018f4b38b23e95f7ae674
rivolt0421 <dlrkddnr0421@daum.net>
2024-09-27 09:42:53 +0900

    PointService 단위 테스트 작성

--- src/point/point.service.unit.spec.ts
+++ src/point/point.service.unit.spec.ts
@@ -2,16 +2,28 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PointService } from './point.service';
 import { UserPointRepositoryToken } from './user-point.repository';
 import { PointHistoryRepositoryToken } from './point-history.repository';
+import { LockManager } from '../lock-manager/lock-manager';
 
+/**
+ * 포인트 서비스의 주요 기능인 충전, 사용, 조회 기능을 테스트합니다.
+ *
+ * 조회
+ *  - 유저의 포인트 정보가 존재하지 않으면, 초기값이 반환됩니다.
+ *  - 충전 시 유저의 포인트 정보가 존재하지 않으면 새롭게 생성하기 때문에,
+ *    조회 시 유저의 포인트 정보가 존재하지 않더라도 에러를 발생시키지 않고 고의적으로 초기값을 반환하도록 했습니다.
+ *
+ * 사용
+ *  - 사용 요청 금액이 현재 포인트 잔액보다 크면 에러를 발생시킵니다.
+ */
 describe('PointService 테스트', () => {
   let pointService: PointService;
   let userPointRepository: any;
-  let pointHistoryRepository: any;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         PointService,
+        LockManager,
         {
           provide: UserPointRepositoryToken,
           useValue: {
@@ -30,7 +42,6 @@ describe('PointService 테스트', () => {
 
     pointService = module.get<PointService>(PointService);
     userPointRepository = module.get(UserPointRepositoryToken);
-    pointHistoryRepository = module.get(PointHistoryRepositoryToken);
   });
 
   describe('조회', () => {
